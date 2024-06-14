abcfe40bb1d175f2c4283188f7b86d711b0c9c98
mahboobe-ziaee <mahboobe.ziaee2020@gmail.com>
2024-06-14 14:13:02 +0430

    added files commit

--- HW_W4_01/obj/Debug/net8.0/HW_W4_01.AssemblyInfo.cs
+++ HW_W4_01/obj/Debug/net8.0/HW_W4_01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HW_W4_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+218a02732df8a043ccb071c66cbfb58b70b607f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HW_W4_01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HW_W4_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
